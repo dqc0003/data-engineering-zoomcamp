f8443220058b1c1d07a77dac2c949204ff07b6b0
Darrel Chew <dqc0003@gmail.com>
2025-08-16 18:36:36 +0000

    add comment

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
@@ -58,3 +58,4 @@ inner join dim_zones as pickup_zone
 on trips_unioned.pickup_locationid = pickup_zone.locationid
 inner join dim_zones as dropoff_zone
 on trips_unioned.dropoff_locationid = dropoff_zone.locationid
+-- daf
